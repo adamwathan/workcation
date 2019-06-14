b09334f81bfa2fa2089cd71e334fe454cbbdfbf4
Adam Wathan <adam.wathan@gmail.com>
2019-06-14 16:16:58 -0400

    Fix vertical scrolling weirdness in Safari

--- src/App.vue
+++ src/App.vue
@@ -9,8 +9,8 @@
             <h3 class="text-gray-900 text-xl">{{ location.title }}</h3>
             <p class="text-gray-600">{{ location.description }}</p>
           </div>
-          <div class="mt-6 sm:overflow-x-auto">
-            <div class="px-4 sm:inline-flex sm:pb-8 xl:px-8">
+          <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
+            <div class="px-4 sm:inline-flex sm:pt-2 sm:pb-8 xl:px-8">
               <div v-for="(property, i) in location.properties" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
                 <PropertyCard :property="property"/>
               </div>
