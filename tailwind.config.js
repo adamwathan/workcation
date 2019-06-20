adbc0ff362ae6e9e7982c4b1e3f3affc40bcd1eb
Adam Wathan <adam.wathan@gmail.com>
2019-06-20 15:46:42 -0400

    Simplify configuration passed to plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,82 +11,33 @@ module.exports = {
     },
     customForms: theme => ({
       default: {
-        'input, textarea': {
-          foo: 'bar',
+        'input, textarea, multiselect, select': {
           lineHeight: theme('lineHeight.snug'),
           color: theme('colors.white'),
-          backgroundColor: theme('colors.gray.900'),
           borderColor: 'transparent',
           borderRadius: theme('borderRadius.lg'),
+          '&:focus': {
+            borderColor: undefined,
+            boxShadow: undefined,
+          },
+        },
+        'input, textarea, multiselect': {
+          backgroundColor: theme('colors.gray.900'),
+          '&:focus': {
+            color: theme('colors.gray.900'),
+            backgroundColor: theme('colors.white'),
+          },
+        },
+        'input, textarea': {
           '&::placeholder': {
             color: theme('colors.gray.600'),
           },
-          '&:focus': {
-            borderColor: undefined,
-            boxShadow: undefined,
-            color: theme('colors.gray.900'),
-            backgroundColor: theme('colors.white'),
-            placeholder: {
-              color: theme('colors.gray.500'),
-            },
-          },
-          '&:hover': {
-            borderColor: theme('colors.red.600'),
+          '&:focus::placeholder': {
+            color: theme('colors.gray.500'),
           },
         },
-
-        textarea: {
-          lineHeight: theme('lineHeight.snug'),
-          color: theme('colors.white'),
-          backgroundColor: theme('colors.gray.900'),
-          borderColor: 'transparent',
-          borderRadius: theme('borderRadius.lg'),
-          placeholder: {
-            color: theme('colors.gray.600'),
-          },
-          '&:focus': {
-            borderColor: undefined,
-            boxShadow: undefined,
-            color: theme('colors.gray.900'),
-            backgroundColor: theme('colors.white'),
-            placeholder: {
-              color: theme('colors.gray.500'),
-            },
-          },
-          '&:hover': {
-            borderColor: theme('colors.red.600'),
-          },
-        },
-
-        multiselect: {
-          lineHeight: theme('lineHeight.snug'),
-          color: theme('colors.white'),
-          backgroundColor: theme('colors.gray.900'),
-          borderColor: 'transparent',
-          borderRadius: theme('borderRadius.lg'),
-          placeholder: {
-            color: theme('colors.gray.600'),
-          },
-          '&:focus': {
-            borderColor: undefined,
-            boxShadow: undefined,
-            color: theme('colors.gray.900'),
-            backgroundColor: theme('colors.white'),
-            placeholder: {
-              color: theme('colors.gray.500'),
-            },
-          },
-          '&:hover': {
-            borderColor: theme('colors.red.600'),
-          },
-        },
-
         select: {
-          lineHeight: theme('lineHeight.snug'),
-          color: theme('colors.white'),
           backgroundColor: theme('colors.gray.700'),
-          borderColor: 'transparent',
-          borderRadius: theme('borderRadius.lg'),
           boxShadow: theme('boxShadow.default'),
           iconColor: '#fff',
           icon: iconColor => `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="${iconColor}"><path d="M17.62 10H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H8.5c-1.2 0-2.3-.72-2.74-1.79l-3.5-7-.03-.06A3 3 0 0 1 5 9h5V4c0-1.1.9-2 2-2h1.62l4 8zM16 11.24L12.38 4H12v7H5a1 1 0 0 0-.93 1.36l3.5 7.02a1 1 0 0 0 .93.62H16v-8.76zm2 .76v8h2v-8h-2z"/></svg>`,
@@ -95,12 +46,8 @@ module.exports = {
             boxShadow: undefined,
             backgroundColor: theme('colors.gray.600'),
           },
-          '&:hover': {
-            borderColor: theme('colors.red.600'),
-          },
         },
-
-        checkbox: {
+        'checkbox, radio': {
           backgroundColor: theme('colors.gray.900'),
           borderColor: 'transparent',
           height: '1.5em',
@@ -109,32 +56,6 @@ module.exports = {
             borderColor: undefined,
             boxShadow: undefined,
             backgroundColor: theme('colors.gray.700'),
-          },
-          '&:hover': {
-            borderColor: theme('colors.red.600'),
-          },
-          '&:checked': {
-            iconColor: '#fff',
-            backgroundColor: theme('colors.indigo.500'),
-            borderColor: 'transparent',
-            '&:focus': {
-              backgroundColor: theme('colors.indigo.400'),
-            }
-          }
-        },
-
-        radio: {
-          backgroundColor: theme('colors.gray.900'),
-          borderColor: 'transparent',
-          height: '1.5em',
-          width: '1.5em',
-          '&:focus': {
-            borderColor: undefined,
-            boxShadow: undefined,
-            backgroundColor: theme('colors.gray.700'),
-          },
-          '&:hover': {
-            borderColor: theme('colors.red.600'),
           },
           '&:checked': {
             iconColor: '#fff',
