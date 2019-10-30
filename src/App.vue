5faf55ccdfcb48dc9040fd9e2d20c644be9f8e03
Kendall Jackson <kendallkj00@gmail.com>
2019-10-30 13:31:08 -0400

    Changed :key to primitive value

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@
     <div class="xl:flex-1 xl:flex xl:overflow-y-hidden">
       <SearchFilters/>
       <main class="py-6 xl:flex-1 xl:overflow-x-hidden">
-        <div v-for="(location, i) in locations" :key="location" :class="{'mt-6': i > 0}">
+        <div v-for="(location, i) in locations" :key="i" :class="{'mt-6': i > 0}">
           <div class="px-4 xl:px-8">
             <h3 class="text-gray-900 text-xl">{{ location.title }}</h3>
             <p class="text-gray-600">{{ location.description }}</p>
           </div>
           <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
             <div class="px-4 sm:inline-flex sm:pt-2 sm:pb-8 xl:px-8">
-              <div v-for="(property, i) in location.properties" :key="property" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
+              <div v-for="(property, i) in location.properties" :key="i" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
                 <PropertyCard :property="property"/>
               </div>
             </div>
