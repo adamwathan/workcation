110c6e14cec4beafc79bcaa8c1137b97938987bc
Kendall Jackson <kendallkj00@gmail.com>
2019-10-30 15:06:07 -0400

    Changed second key to j
    :key on second v-for changed to 'j' because 'i' is used by the parent. Final fix for the issue.

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@
           </div>
           <div class="mt-6 sm:overflow-x-auto sm:overflow-y-hidden">
             <div class="px-4 sm:inline-flex sm:pt-2 sm:pb-8 xl:px-8">
-              <div v-for="(property, i) in location.properties" :key="i" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
+              <div v-for="(property, j) in location.properties" :key="j" :class="{'mt-10 sm:ml-4': i > 0 }" class="sm:mt-0 sm:w-80 sm:flex-shrink-0">
                 <PropertyCard :property="property"/>
               </div>
             </div>
